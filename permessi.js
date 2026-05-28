79d657ef815970531aab82f2e422900b113c9df2
Alessandra De Gaetano <136568475+alessandraDGinf@users.noreply.github.com>
2026-05-28 20:36:49 +0200

    Update permessi.js

--- permessi.js
+++ permessi.js
@@ -7,7 +7,7 @@ const DOMINIO_SCUOLA = "panettipitagora.edu.it";
 // Super admin: accesso completo a tutto + pannello admin
 const SUPER_ADMIN_EMAILS = [
 "alessandra.degaetano@panettipitagora.edu.it",
-"dirigente@panettipitagora.edu.it",
+"dirigente@panettipitagora.edu.it"//,
   "giuseppe.mastrandrea@panettipitagora.edu.it"
 ];
 
@@ -26,6 +26,14 @@ const DB_DOCENTI = {
     classi: ["1A", "2A", "2E", "2I"],
     materie: ["INGLESE"],
     coordinatore: [],
+    sostegno: false,
+    superAdmin: false
+  },
+  "giuseppe.mastrandrea@panettipitagora.edu.it": {
+    nome: "Mastrandrea Giuseppe",
+    classi: ["1G", "2G"],
+    materie: ["ITALIANO", "STORIA"],
+    coordinatore: ["1G"],
     sostegno: false,
     superAdmin: false
   },
